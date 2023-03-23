cbd64a02275b870268d4453c69038e224c490e13
Pietro Polinari Cavassin <ppc19@inf.ufpr.br>
2023-03-22 21:49:30 -0300

    No bugs and much faster version

--- 1_compositing_images/project1.js
+++ 1_compositing_images/project1.js
@@ -1,49 +1,47 @@
+// Made by Pietro Polinari Cavassin
+
 // bgImg is the background image to be modified.
 // fgImg is the foreground image.
 // fgOpac is the opacity of the foreground image.
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
 function composite( bgImg, fgImg, fgOpac, fgPos )
-{
-    console.log(fgImg.data[3])
-    var bgX, bgY, bgIdx;
-    var cnt = 0;
-    for (var fgIdx = 0; fgIdx < fgImg.data.length; fgIdx+=4){ 
-        // check if this pixel overlaps background image
-        bgX = Math.floor((fgIdx/4) % fgImg.width + fgPos.x);
-        bgY = Math.floor((fgIdx/4) / fgImg.width + fgPos.y);
-        
-        if (outOfRange(bgX, bgY, bgImg.width, bgImg.height))
-            continue;
-        // we can optimize this A LOT only iterating through the intersecting part
+{   
+    // calculate intersection between foreground and background
+    var lowX = Math.max(fgPos.x, 0);
+    var lowY = Math.max(fgPos.y, 0);
+    var highX = Math.min(fgPos.x + fgImg.width, bgImg.width);
+    var highY = Math.min(fgPos.y + fgImg.height, bgImg.height);
 
-        // calculate pixel index in background image array
-        bgIdx = (bgY * bgImg.width + bgX) * 4;
+    // Iterate only through pixels in intersection.
+    var fgX, fgY, bgX, bgY, bgIdx, fgIdx;
+    for (bgX = lowX; bgX < highX; ++bgX){
+        for (bgY = lowY; bgY < highY; ++bgY){
+            fgX = bgX - fgPos.x;
+            fgY = bgY - fgPos.y;
 
-        // calculate new rgba 
-        if (fgOpac == 0) cnt+=1;
-        
-        calculateNewColor(bgImg.data, bgIdx, fgImg.data, fgIdx, fgOpac)
+            // Convert image x and y coordinates to index in pixel array
+            bgIdx = (bgY * bgImg.width + bgX) * 4; 
+            fgIdx = (fgY * fgImg.width + fgX) * 4; 
+
+            calculateNewColor(bgImg.data, bgIdx, fgImg.data, fgIdx, fgOpac);
+        }
     }
 }
 
+// This is where all the magic happens.
 function calculateNewColor(bgData, bgIdx, fgData, fgIdx, fgOpac){
-    var bgColor = bgData.slice(bgIdx, bgIdx+4);
-    var fgColor = fgData.slice(fgIdx, fgIdx+4);
-    
-    var fgAlpha = Number(fgColor[3])/255 * fgOpac;
-    var bgAlpha = Number(bgColor[3])/255;
+    // normalize alphas between 0 and 1
+    var bgAlpha = bgData[bgIdx+3]/255;
+    var fgAlpha = fgData[fgIdx+3]/255 * fgOpac;
 
     // calculate alpha channel
-    bgData[bgIdx+3] = fgAlpha + (1-fgAlpha)*bgAlpha; // alpha
-    if (bgData[bgIdx+3] === 0.0)  return;
+    var alpha = fgAlpha + (1-fgAlpha)*bgAlpha;
+    if (alpha === 0.0) return;
 
-    // calculate RGB channels
-    for (var c = 0; c < 3; ++c){
-        bgData[bgIdx+c] = Math.round((fgAlpha*fgColor[c] + (1-fgAlpha)*bgColor[c]*bgAlpha) / bgData[bgIdx+3])
-    }
-    bgData[bgIdx+3] = Math.round(bgData[bgIdx+3]*255)
-}
-
-function outOfRange(x, y, width, height){
-    return !(x >= 0 && x < width && y >= 0 && y < height)
+    // calculate and update RGB channels
+    for (var c = 0; c < 3; ++c)
+        bgData[bgIdx+c] = (fgAlpha*fgData[fgIdx+c] + (1-fgAlpha)*bgData[bgIdx+c]*bgAlpha) / alpha;
+    
+    // update alpha channel
+    bgData[bgIdx+3] = alpha*255;
 }
